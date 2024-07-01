b2df66b74f85cd1f2c968cfb7e38bbc1f606578f
mickeygard <138319565+mickeygard@users.noreply.github.com>
2024-06-30 22:58:40 -0500

    Update main.jsx
    Fixed import error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App, Grid, Box from './App.jsx'
+import App from './App.jsx'
+import Grid from './App.jsx'
+import Box from './App.jsx'
 import './index.css'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
